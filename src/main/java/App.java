b3c5e4af8ebb1e76bfabf2e283d63bfd6b417adf
Perry Eising <Perry.R.Eising@gmail.com>
2017-08-02 15:36:44 -0700

    completed adding 3 restaurant routes, postman testing

---
+++ src/main/java/App.java
@@ -0,0 +1,49 @@
+import com.google.gson.Gson;
+import dao.Sql2oFoodtypeDao;
+import dao.Sql2oRestaurantDao;
+import dao.Sql2oReviewDao;
+import models.Restaurant;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import static spark.Spark.*;
+
+public class App {
+
+    public static void main(String[] args) {
+        Sql2oFoodtypeDao foodtypeDao;
+        Sql2oRestaurantDao restaurantDao;
+        Sql2oReviewDao reviewDao;
+        Connection conn;
+        Gson gson = new Gson();
+
+        String connectionString = "jdbc:h2:~/jadle.db;INIT=RUNSCRIPT from 'classpath:db/create.sql'"; //check me!
+        Sql2o sql2o = new Sql2o(connectionString, "", "");
+        restaurantDao = new Sql2oRestaurantDao(sql2o);
+        foodtypeDao = new Sql2oFoodtypeDao(sql2o);
+        reviewDao = new Sql2oReviewDao(sql2o);
+        conn = sql2o.open();
+
+        //CREATE
+        post("/restaurants/new", "application/json", (req, res) -> { //accept a request in format JSON from an app
+            Restaurant restaurant = gson.fromJson(req.body(), Restaurant.class);//make java from JSON with GSON
+            restaurantDao.add(restaurant);//Do our thing with our DAO
+            res.status(201);//everything went well - update the response status code
+            res.type("application/json");
+            return "The following was added to the API\n\r" + gson.toJson(restaurant);//send it back to be displayed
+        });
+
+        get("/restaurants", "application/json", (req, res) -> { //accept a request in format JSON from an app
+            res.type("application/json");
+            return gson.toJson(restaurantDao.getAll());//send it back to be displayed
+        });
+
+        get("/restaurants/:id", "application/json", (req, res) -> { //accept a request in format JSON from an app
+            res.type("application/json");
+            int restaurantId = Integer.parseInt(req.params("id"));
+            res.type("application/json");
+            return gson.toJson(restaurantDao.findById(restaurantId));
+        });
+
+
+    }
+}
