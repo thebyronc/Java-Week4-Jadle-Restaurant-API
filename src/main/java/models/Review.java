5d38b3d21c67a1aaf7e88ea7dc77096d5d841477
Perry Eising <perry.r.eising@gmail.com>
2018-01-04 15:34:35 -0800

    correct Review model

--- src/main/java/models/Review.java
+++ src/main/java/models/Review.java
@@ -1,18 +1,58 @@
 package models;
 
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
-
 public class Review {
 
+    private int id;
     private String writtenBy;
     private int rating;
-    private int id;
+    private String content;
     private int restaurantId;
 
-    public Review(String writtenBy, int rating, int restaurantId) {
+    public Review(String writtenBy, int rating, String content, int restaurantId) {
         this.writtenBy = writtenBy;
         this.rating = rating;
+        this.content = content;
+        this.restaurantId = restaurantId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getWrittenBy() {
+        return writtenBy;
+    }
+
+    public void setWrittenBy(String writtenBy) {
+        this.writtenBy = writtenBy;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+        this.restaurantId = restaurantId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getRestaurantId() {
+        return restaurantId;
+    }
+
+    public void setRestaurantId(int restaurantId) {
         this.restaurantId = restaurantId;
     }
 
@@ -24,16 +64,16 @@ public class Review {
         Review review = (Review) o;
 
         if (rating != review.rating) return false;
-        if (id != review.id) return false;
         if (restaurantId != review.restaurantId) return false;
-        return writtenBy != null ? writtenBy.equals(review.writtenBy) : review.writtenBy == null;
+        if (!writtenBy.equals(review.writtenBy)) return false;
+        return content.equals(review.content);
     }
 
     @Override
     public int hashCode() {
-        int result = writtenBy != null ? writtenBy.hashCode() : 0;
+        int result = writtenBy.hashCode();
         result = 31 * result + rating;
-        result = 31 * result + id;
+        result = 31 * result + content.hashCode();
         result = 31 * result + restaurantId;
         return result;
     }
