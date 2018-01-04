0622d78b047a7e990f2d88b5e2ca3ec19f0ee1ab
Perry Eising <perry.r.eising@gmail.com>
2018-01-04 15:34:35 -0800

    correct Review model

--- src/main/java/models/Review.java
+++ src/main/java/models/Review.java
@@ -1,40 +1,18 @@
 package models;
 
-import java.time.LocalDateTime;
-
 public class Review {
 
     private String writtenBy;
     private int rating;
-    private LocalDateTime createdAt;
     private int id;
+    private String content;
     private int restaurantId;
 
-    public Review(String writtenBy, int rating, int restaurantId) {
+    public Review(String writtenBy, int rating, String content, int restaurantId) {
         this.writtenBy = writtenBy;
         this.rating = rating;
+        this.content = content;
         this.restaurantId = restaurantId;
-        this.createdAt = Timestamp.valueOf(LocalDateTime.now());
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Review review = (Review) o;
-
-        if (rating != review.rating) return false;
-        if (id != review.id) return false;
-        return writtenBy.equals(review.writtenBy);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = writtenBy.hashCode();
-        result = 31 * result + rating;
-        result = 31 * result + id;
-        return result;
     }
 
     public String getWrittenBy() {
@@ -53,19 +31,49 @@ public class Review {
         this.rating = rating;
     }
 
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
     public int getId() {
         return id;
     }
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getRestaurantId() {
+        return restaurantId;
+    }
+
+    public void setRestaurantId(int restaurantId) {
+        this.restaurantId = restaurantId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Review review = (Review) o;
+
+        if (rating != review.rating) return false;
+        if (restaurantId != review.restaurantId) return false;
+        if (!writtenBy.equals(review.writtenBy)) return false;
+        return content.equals(review.content);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = writtenBy.hashCode();
+        result = 31 * result + rating;
+        result = 31 * result + content.hashCode();
+        result = 31 * result + restaurantId;
+        return result;
     }
 }
