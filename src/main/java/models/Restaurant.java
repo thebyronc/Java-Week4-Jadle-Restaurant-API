dda694a13e1fe9573011c7bdf5bd46f9bbbc172a
Perry Eising <perry.r.eising@gmail.com>
2018-01-04 10:37:09 -0800

    remove enum and references to enum

--- src/main/java/models/Restaurant.java
+++ src/main/java/models/Restaurant.java
@@ -1,8 +1,5 @@
 package models;
 
-
-import enums.DiningStyle;
-
 public class Restaurant {
 
     private String name;
@@ -12,56 +9,25 @@ public class Restaurant {
     private String website;
     private String email;
     private int id;
-    private DiningStyle diningStyle;
 
-    public Restaurant(String name, String address, String zipcode, String phone, DiningStyle diningStyle) {
+    public Restaurant(String name, String address, String zipcode, String phone) {
         this.name = name;
         this.address = address;
         this.zipcode = zipcode;
         this.phone = phone;
         this.website = "no website listed";
         this.email = "no email available";
-        this.diningStyle = diningStyle;
 
     }
 
-    public Restaurant(String name, String address, String zipcode, String phone, String website, String email,  DiningStyle diningStyle) {
+    public Restaurant(String name, String address, String zipcode, String phone, String website, String email) {
         this.name = name;
         this.address = address;
         this.zipcode = zipcode;
         this.phone = phone;
         this.website = website;
         this.email = email;
-        this.diningStyle = diningStyle;
    }
-
-    public String getDiningStyle(){
-        String message;
-        switch (this.diningStyle){
-            case CASUAL:
-                message =  String.format("%s likes to keep things pretty casual. No stuffy suits here!", this.getName());
-            break;
-
-            case FASTFOOD:
-               message = String.format("%s is a fast food restaurant. Don't expect ambiance.", this.getName());
-                break;
-
-            case FOODCART:
-                message =  String.format("%s is part of the Portland Foodcart scene. Bring an umbrella if its raining!", this.getName());
-                break;
-
-            case FINEDINING:
-                message =  String.format("Get ready to spend some coin at %s. Don't arrive underdressed - it's posh.", this.getName());
-                break;
-
-           default:
-               message =  ("Something went wrong. Please try again.");
-               break;
-        }
-        return message;
-    }
-
-
 
     public String getName() {
         return name;
@@ -111,7 +77,6 @@ public class Restaurant {
         this.email = email;
     }
 
-
     public int getId() {
         return id;
     }
@@ -119,6 +84,7 @@ public class Restaurant {
     public void setId(int id) {
         this.id = id;
     }
+
 
     @Override
     public boolean equals(Object o) {
@@ -131,7 +97,8 @@ public class Restaurant {
         if (!address.equals(that.address)) return false;
         if (!zipcode.equals(that.zipcode)) return false;
         if (!phone.equals(that.phone)) return false;
-        return diningStyle == that.diningStyle;
+        if (website != null ? !website.equals(that.website) : that.website != null) return false;
+        return email != null ? email.equals(that.email) : that.email == null;
     }
 
     @Override
@@ -140,7 +107,8 @@ public class Restaurant {
         result = 31 * result + address.hashCode();
         result = 31 * result + zipcode.hashCode();
         result = 31 * result + phone.hashCode();
-        result = 31 * result + diningStyle.hashCode();
+        result = 31 * result + (website != null ? website.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
         return result;
     }
 }
